f6a89ffa41eedc31243883c37c5d1cfda8fe48ad
mo <thinkgo@aliyun.com>
2020-07-23 20:24:41 +0800

    增加option

---
+++ 11_opiton/src/main.rs
@@ -0,0 +1,50 @@
+//1.Option是标准库定义的一个枚举，形式：
+//enum Option<T> {
+//  Some(T),
+//  None,
+//}
+//
+//2.使用方式
+fn main() {
+    let some_number = Some(5);
+    let some_string = Some(String::from("a string"));
+    let absent_number: Option<i32> = None;
+
+    let x: i32 = 5;
+    let y: Option<i32> = Some(5);
+    let mut temp = 0;
+    match y {
+        Some(i) => {
+            temp = i;
+        }
+        None => {
+            println!("do nothing");
+        }
+    }
+
+    let sum = x + temp;
+    println!("sum = {}", sum);
+
+    //let result = plus_one(y);
+    //match result {
+    //    Some(i) => println!("value = {}", i),
+    //    None => println!("nothing"),
+    //};
+    // 等价于上面,可忽略None
+    if let Some(value) = plus_one(y) {
+        println!("value = {}", value);
+    }
+
+    if let Some(value) = plus_one(y) {
+        println!("value = {}", value);
+    } else {
+        println!("do nothing");
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(x) => Some(x + 1),
+    }
+}
