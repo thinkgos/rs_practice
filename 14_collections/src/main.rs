38c75760a729985c73427317ae7653330696f0a2
mo <thinkgo@aliyun.com>
2020-08-28 10:55:55 +0800

    add collections

---
+++ 14_collections/src/main.rs
@@ -0,0 +1,32 @@
+use std::collections::HashMap;
+
+fn main() {
+    // 新建空的vector
+    let mut v: Vec<i32> = Vec::new();
+    v.push(5);
+    v.push(6);
+    v.push(7);
+    v.push(8);
+    println!("{}", v.get(1).unwrap());
+
+    // 采用vec宏
+    let v2 = vec![1, 2, 3, 4];
+
+    // hash map
+    let mut ss1 = HashMap::new();
+    ss1.insert(String::from("one"), 1);
+    ss1.insert(String::from("two"), 2);
+    ss1.insert(String::from("three"), 3);
+    ss1.entry(String::from("one")).or_insert(3);
+    println!("ss1 = {:?}", ss1);
+
+    //根据旧值来更新一个值
+    let text = "hello world wonderful world";
+    let mut map = HashMap::new();
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+    println!("map = {:?}", map);
+    println!("Hello, world!");
+}
