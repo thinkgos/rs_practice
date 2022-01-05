2c477c7466dcd7da680cf697045db77090379710
thinkgo <thinkgo@aliyun.com>
2022-01-05 19:46:33 +0800

    update typeinto

---
+++ 16_typeinto/src/main.rs
@@ -0,0 +1,19 @@
+use std::convert::TryInto;
+
+fn main() {
+    let a: i32 = 10;
+    let b: u16 = 100;
+
+    if a < (b as i32) {
+        println!("Ten is less than one hundred.");
+    }
+
+    let a: u8 = 10;
+    let b: u16 = 1500;
+
+    let a_: u16 = a.try_into().unwrap();
+
+    if a_ < b {
+        println!("Ten is less than one hundred.");
+    }
+}
