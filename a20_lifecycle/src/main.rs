24528ed48bd41a795a9eaf9c20f6a39e25d77af0
thinkgo <thinkgo@aliyun.com>
2022-05-06 21:08:25 +0800

    fix

--- a20_lifecycle/src/main.rs
+++ a20_lifecycle/src/main.rs
@@ -2,7 +2,7 @@ fn main() {
     let string1 = String::from("abcd");
     let string2 = String::from("xyz");
 
-    let result = longest(string1.as_str(), string2.as_str());
+    let result = longest(&string1, string2.as_str());
 
     println!("The longest string is {}", result);
 }
