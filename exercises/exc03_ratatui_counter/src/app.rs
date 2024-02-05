e0abda0801294e1950b22cf00209e18236feaa8f
thinkgos <thinkgo@aliyun.com>
2024-02-05 17:11:24 +0800

    chore: add tui

---
+++ exercises/exc03_ratatui_counter/src/app.rs
@@ -0,0 +1,33 @@
+/// Application.
+#[derive(Debug, Default)]
+pub struct App {
+    /// should the application exit?
+    pub should_quit: bool,
+    /// counter
+    pub counter: u8,
+}
+
+impl App {
+    /// Constructs a new instance of [`App`].
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    /// Handles the tick event of the terminal.
+    pub fn tick(&self) {}
+
+    /// Set should_quit to true to quit the application.
+    pub fn quit(&mut self) {
+        self.should_quit = true;
+    }
+    pub fn increment_counter(&mut self) {
+        if let Some(res) = self.counter.checked_add(1) {
+            self.counter = res;
+        }
+    }
+    pub fn decrement_counter(&mut self) {
+        if let Some(res) = self.counter.checked_sub(1) {
+            self.counter = res;
+        }
+    }
+}
---
+++ exercises/exc03_ratatui_counter/src/main.rs
@@ -0,0 +1,36 @@
+/// Application.
+pub mod app;
+/// Terminal events handler.
+pub mod event;
+/// Terminal user interface.
+pub mod tui;
+/// Widget renderer.
+pub mod ui;
+/// Application updater.
+pub mod update;
+
+use anyhow::Result;
+use app::App;
+use event::{Event, EventHandler};
+use tui::Tui;
+use update::update;
+
+fn main() -> Result<()> {
+    // Create an application.
+    let mut app = App::new();
+    let mut tui = Tui::new()?;
+    let events = EventHandler::new(250);
+    // Start the main loop.
+    while !app.should_quit {
+        // Render the user interface.
+        tui.draw(|frame| app.render(frame))?;
+        // Handle events.
+        match events.next()? {
+            Event::Tick => {}
+            Event::Key(key_event) => update(&mut app, key_event),
+            Event::Mouse(_) => {}
+            Event::Resize(_, _) => {}
+        };
+    }
+    Ok(())
+}
